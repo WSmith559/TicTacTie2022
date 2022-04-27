dec15da7e8248e89f23a9b0f33867c75c2d0b683
DESKTOP-MLFNFSA\willi <wlsmith@perse.co.uk>
2022-04-27 15:56:57 +0100

    end of lesson working on MVP

--- Game.py
+++ Game.py
@@ -1,18 +1,28 @@
-
 class Game:
 
+    EMPTY = " "
+    P1 = "O"
+    P2 = "X"
+
     def __init__(self):
-        pass
+        self.__board = [[Game.EMPTY for _ in range(3)] for _ in range(3)]
+        self.__player = Game.P1
 
     def __repr__(self):
-        pass
+        return("this is the board")
 
     def play(self,row,col):
-        pass
+        row -= 1
+        col -= 1
+        self.__board[row][col] = self.__player
+        if self.__player == Game.P1:
+            self.__player = Game.P2
+        elif self.__player == Game.P2:
+            self.__player = Game.P1
     
     @property
     def winner(self):
-        pass
-
+        return None
+        
 if __name__ == "__main__":
     pass
--- Ui.py
+++ Ui.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from Game import Game
 
 class Ui(ABC):
 
@@ -15,7 +16,17 @@ class Gui(Ui):
 
 class Terminal(Ui):
     def __init__(self):
-        pass
+        self.__Game = Game()
+
+    def __get_input(self):
+        row = int(input("enter row"))
+        col = int(input("enter column"))
+        return row, col
 
     def run(self):
-        pass
+        while self.__Game.winner == None:
+            print(self.__Game)
+            row, col = self.__get_input()
+            self.__game.play(row, col)
+        
+        print(f"the winner is {self.__Game.winner}")

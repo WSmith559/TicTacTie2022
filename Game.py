6a38c521fbe447db4e671724900d72916663415b
DESKTOP-MLFNFSA\willi <wlsmith@perse.co.uk>
2022-04-28 14:51:13 +0100

    Added Draw and added inability to overwrite square

--- Game.py
+++ Game.py
@@ -1,8 +1,14 @@
+from itertools import product
+
+class GameError(Exception):
+    pass
+
 class Game:
 
     EMPTY = " "
     P1 = "O"
     P2 = "X"
+    DRAW = "draw"
 
     def __init__(self):
         self.__board = [[Game.EMPTY for _ in range(3)] for _ in range(3)]
@@ -21,6 +27,9 @@ class Game:
     def play(self,row,col):
         row -= 1
         col -= 1
+        if self.__board[row][col] != Game.EMPTY:
+            raise GameError("cannot play here")
+
         self.__board[row][col] = self.__player
         if self.__player == Game.P1:
             self.__player = Game.P2
@@ -40,6 +49,8 @@ class Game:
                     return p
                 if all(self.__board[2-i][i] == p for i in range(3)):
                     return p
+        if not any(self.__board[row][col] == Game.EMPTY for row,col in product(range(3),range(3))):
+            return Game.DRAW
         return None
 if __name__ == "__main__":
     pass
--- Ui.py
+++ Ui.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from Game import Game
+from Game import Game, GameError
 
 class Ui(ABC):
 
@@ -19,15 +19,30 @@ class Terminal(Ui):
         self.__Game = Game()
 
     def __get_input(self):
-        row = int(input("enter row "))
-        col = int(input("enter column "))
+        while True:
+            try: #type and range check
+                row = int(input("enter row "))
+                col = int(input("enter column "))
+                if 1 <= row <= 3 and 1<= col <= 3:
+                    break
+                else:
+                    print("Invalid Input, Try Again")
+                break
+            except ValueError:
+                print("Invalid Input, Try Again")
         return row, col
 
     def run(self):
         while self.__Game.winner == None:
             print(self.__Game)
             row, col = self.__get_input()
-            self.__Game.play(row, col)
-
+            try:
+                self.__Game.play(row, col)
+            except GameError:
+                pass
+        
         print(self.__Game)
-        print(f"the winner is {self.__Game.winner}")
+        if self.__Game.winner == Game.DRAW:
+            print("game is a draw")
+        else:
+            print(f"the winner is {self.__Game.winner}")
